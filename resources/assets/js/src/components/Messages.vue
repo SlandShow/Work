09d83d9ada845e9f595adf539c66451fd9780aba
Albert <higashi166@gmail.com>
2018-04-18 21:10:16 +0200

    Added Vue components

---
+++ resources/assets/js/src/components/Messages.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="messages">
+        <message 
+          v-for='message in messages'
+          v-bind:key="message.id"
+        ></message>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Message from './Message'
+    export default {
+        name: 'messages',
+        data () {
+            return {
+                messages: [1],
+                data: {}
+            }
+        },
+        mounted () {
+            this.get_messages()
+        },
+        methods: {
+            // FIXME:
+            get_messages: function(){
+                this.intervalid1 = setInterval(() => {
+                    console.log('get')
+                    this.messages.push('3')
+                    axios.get('api/mark_read?id={}')
+                      .then((responce) => {
+                      this.data = responce.data
+                      })
+                      .catch((error) => {
+                      console.log(error)
+                    })
+                }, 10000)
+            }
+        },
+        components: {
+            Message
+        } 
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/assets/js/src/main.js
+++ resources/assets/js/src/main.js
@@ -5,9 +5,9 @@ import App from './App'
 import router from './router'
 
 import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
